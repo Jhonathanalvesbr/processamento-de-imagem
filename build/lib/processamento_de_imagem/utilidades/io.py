3b5ede418085291e52ebcd5008dbb06eeff66d7b
Jhonathanalvesbr <jhonathanalves_br@hotmail.com>
2022-03-07 20:09:03 -0300

    main

--- build/lib/processamento_de_imagem/utilidades/io.py
+++ build/lib/processamento_de_imagem/utilidades/io.py
@@ -1,8 +1,8 @@
 from skimage.io import imread, imsave
 
-def read_imagem(caminho, cinza = False):
+def ler_imagem(caminho, cinza = False):
     imagem = imread(caminho, as_gray = cinza)
     return imagem
 
-def save_imagem(imagem, caminho):
+def salvar_imagem(imagem, caminho):
     imsave(caminho, imagem) 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="processamento_de_imagem",
-    version="0.0.1",
+    version="0.0.2",
     author="Jhonathan Alves",
     description="Processamento de imagem usando combinação de histograma",
     long_description=page_description,
